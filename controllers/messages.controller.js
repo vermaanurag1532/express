e0c17053ed683f843ae79061cb1bbc12f9d816a6
vermaanurag1532 <vermaanurag1532@gmail.com>
2022-10-18 03:29:16 +0530

    adding layout template engine

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -4,7 +4,11 @@ const path = require('path');
 //     res.send('<ul><li>hellow, Anurag Verma this side . <li><ul>');
 // }  // we dont use array suntax to write the function bcoz when there is an error node does not tell us that which function throw that error
 function getMessages(req, res) {
-    res.sendFile(path.join(__dirname, '..', 'public', 'images', 'anurag.jpeg'));
+    res.render('messages', {
+        title: 'messages to my friends',
+        friend: 'Anurag verma',
+    });
+    // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'anurag.jpeg'));
     // res.send('<ul><li>hellow, Anurag Verma this side . <li><ul>');
 } // in this case node tell due to this function an error occur
 
